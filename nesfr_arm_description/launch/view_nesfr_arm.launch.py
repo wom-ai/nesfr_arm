abecbbb2f62453cd7ffad3610434271ca99b8b70
Jaeyoung Lee <jylee0121@gmail.com>
2023-03-03 14:26:06 +0900

    update: update nesfr robot arm urdf and its constraints
    Signed-off-by: Jaeyoung Lee <jylee0121@gmail.com>

--- nesfr_arm_description/launch/view_nesfr_arm.launch.py
+++ nesfr_arm_description/launch/view_nesfr_arm.launch.py
@@ -3,37 +3,45 @@
 
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
+from launch.actions import TimerAction
 from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
-    declared_arguments = []
     # General arguments
-    declared_arguments.append(
-        DeclareLaunchArgument(
-            "description_package",
-            default_value="nesfr_arm_description",
-            description="Description package with robot URDF/XACRO files. Usually the argument \
+    description_package = LaunchConfiguration("description_package")
+    description_package_arg = DeclareLaunchArgument(
+        "description_package",
+        default_value="nesfr_arm_description",
+        description="Description package with robot URDF/XACRO files. Usually the argument \
         is not set, it enables use of a custom description.",
         )
-    )
-    declared_arguments.append(
-        DeclareLaunchArgument(
-            "description_file",
-            default_value="nesfr_arm.urdf.xacro",
-            description="URDF/XACRO description file with the robot.",
+
+    description_file = LaunchConfiguration("description_file")
+    description_file_arg = DeclareLaunchArgument(
+        "description_file",
+        default_value="nesfr_arm.urdf.xacro",
+        description="URDF/XACRO description file with the robot.",
         )
-    )
+
+    robot_name = LaunchConfiguration("robot_name")
+    robot_name_arg = DeclareLaunchArgument(
+        "robot_name",
+        default_value="nesfr7_arm",
+        description="robot parameters",
+        )
 
     # Initialize Arguments
 
     # General arguments
-    description_package = LaunchConfiguration("description_package")
-    description_file = LaunchConfiguration("description_file")
+    nesfr_arm_params_file = LaunchConfiguration('nesfr_arm_params_file', default=[robot_name, '.yaml'])
 
+    #
+    # reference: https://answers.ros.org/question/401950/ros2-any-idea-on-how-to-pass-parameters-yaml-to-a-xacro-file-via-launch-python-script/
+    #
     nesfr_arm_params = PathJoinSubstitution(
-        [FindPackageShare(description_package), "config", "nesfr_arm.yaml"]
+        [FindPackageShare(description_package), "config", nesfr_arm_params_file]
     )
 
     robot_description_content = Command(
@@ -44,6 +52,7 @@ def generate_launch_description():
             " ",
             "nesfr_arm_params:=",
             nesfr_arm_params,
+            " ",
         ]
     )
     robot_description = {"robot_description": robot_description_content}
@@ -72,9 +81,15 @@ def generate_launch_description():
     )
 
     nodes_to_start = [
-        joint_state_publisher_node,
         robot_state_publisher_node,
-        rviz_node,
+        joint_state_publisher_node,
+        # https://answers.ros.org/question/387310/run-ros2-node-using-launch-file-with-delay/
+        # one second delay after setup
+        TimerAction(period=1.0, actions=[rviz_node,]),
     ]
 
-    return LaunchDescription(declared_arguments + nodes_to_start)
+    return LaunchDescription([
+        description_package_arg,
+        description_file_arg,
+        robot_name_arg,
+    ] + nodes_to_start)
